a74a38923a2b07fc7d528d22af15ddd892a093ae
eladdavid5 <elad.david5@gmail.com>
2023-05-08 23:40:41 +0300

    ws update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,40 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Recipes from './pages/Recipes';
 
 
+
+import  { useState, useEffect } from 'react'
+import io from 'socket.io-client';
+
+
 function App() {
+
+  // useEffect(() => {
+  //   const socket = io('ws://localhost:3001', {
+  //     transports: ['websocket']
+  //   });
+  
+  //   socket.on('connect', () => {
+  //     console.log('Socket connected:', socket.id);
+  //   });
+  
+  //   socket.on('disconnect', () => {
+  //     console.log('Socket disconnected');
+  //   });
+  
+  //   socket.on('connect_error', (error) => {
+  //     console.log(`Socket connection error: ${error}`);
+  //   });
+  
+  //   socket.on('greeting', (data) => {
+  //     console.log('Received greeting:', data);
+  //   });
+  
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // }, []);
+  
+  
   return (
     <Router>
       <Routes>
--- src/components/Grid/GroceriesGrid.js
+++ src/components/Grid/GroceriesGrid.js
@@ -19,26 +19,8 @@ function GroceriesGrid(props) {
   const [images, setImages] = useState([])
   const [showRecipe, setShowRecipe] = useState(false)
 
-  useEffect(() => {
-    const socket = io('http://localhost:3000');
-    socket.on('connect', () => {
-      console.log('Socket connected');
-    });
-    socket.on('disconnect', () => {
-      console.log('Socket disconnected');
-    });
-    socket.on('connect_error', (error) => {
-      console.log(`Socket connection error: ${error}`);
-    });
-    socket.on('greeting', (data) => {
-      alert(data); // prints "Hello, client!"
-    });
-  
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-  
+
+
 
   useEffect(() => {
     async function fetchImages() {
