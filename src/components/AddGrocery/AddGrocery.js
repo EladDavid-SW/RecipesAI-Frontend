9b986a85bfe07cb5540a02706d53dd4647493553
eladdavid5 <elad.david5@gmail.com>
2023-09-01 17:09:36 +0300

    add progress icons to create grocery

--- src/components/AddGrocery/AddGrocery.js
+++ src/components/AddGrocery/AddGrocery.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
-import { Grid, Typography, TextField, Button } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { Grid, Typography, TextField, Button, CircularProgress } from '@mui/material'
 import io from 'socket.io-client'
 import './AddGrocery.css'
+import GreenVIconImage from '../../images/checked.png'
 
-const backendURL = process.env.REACT_APP_BACKEND_URL;
+const backendURL = process.env.REACT_APP_BACKEND_URL
 
 const AddGrocery = ({ onSubmit }) => {
   const [newGrocery, setNewGrocery] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [showGreenIcon, setShowGreenIcon] = useState(false)
+
+  useEffect(() => {
+    const errorTimeout = setTimeout(() => {
+      setErrorMessage('')
+    }, 3000)
+
+    return () => {
+      clearTimeout(errorTimeout)
+    }
+  }, [errorMessage])
 
   function uploadImage(imageUrl) {
     const socket = io(backendURL, {
@@ -24,8 +38,18 @@ const AddGrocery = ({ onSubmit }) => {
         if (error) {
           console.log('Error sending uploadImage event:', error)
         }
+
+        setIsLoading(false)
       })
-      socket.disconnect()
+
+      setTimeout(() => {
+        socket.disconnect()
+        setShowGreenIcon(true)
+        setTimeout(() => {
+          setShowGreenIcon(false)
+          onSubmit(false)
+        }, 2000)
+      }, 11000)
     })
 
     socket.on('disconnect', () => {
@@ -39,7 +63,6 @@ const AddGrocery = ({ onSubmit }) => {
     socket.on('greeting', (data) => {
       console.log('Received greeting:', data)
     })
-
   }
 
   function handleNewGroceryChange(event) {
@@ -47,11 +70,16 @@ const AddGrocery = ({ onSubmit }) => {
   }
 
   async function handleNewGrocerySubmit() {
+    if (newGrocery.trim() === '') {
+      setErrorMessage('Please enter a grocery item name before clicking Add.')
+      return
+    }
+    setIsLoading(true)
     const newGroceryItem = { name: newGrocery }
     console.log(newGroceryItem)
     setNewGrocery('')
+
     await uploadImage(newGrocery)
-    onSubmit(false)
   }
 
   return (
@@ -59,12 +87,25 @@ const AddGrocery = ({ onSubmit }) => {
       <Typography variant='h5' style={{ marginRight: '1rem' }}>
         Add Grocery
       </Typography>
-      <TextField label='Grocery Name' value={newGrocery} onChange={handleNewGroceryChange} variant='outlined' style={{ marginRight: '1rem' }} />
-      <Button variant='contained' color='primary' onClick={handleNewGrocerySubmit}>
-        Add
-      </Button>
+      <TextField label='Grocery Name' value={newGrocery} onChange={handleNewGroceryChange} variant='outlined' className={errorMessage ? 'error-input' : ''} style={{ marginRight: '1rem' }} />
+
+      {isLoading ? (
+        showGreenIcon ? (
+          <img src={GreenVIconImage} alt='Green V Icon' style={{ width: '32px', height: '32px', marginLeft: '1rem' }} />
+        ) : (
+          <CircularProgress style={{ marginRight: '1rem' }} />
+        )
+      ) : (
+        <Button variant='contained' color='primary' onClick={handleNewGrocerySubmit}>
+          Add
+        </Button>
+      )}
+      {/* {errorMessage && (
+        <Typography variant='body2' color='error' style={{ margin: '0', marginTop: '8px' }}>
+          {errorMessage}
+        </Typography>
+      )} */}
     </Grid>
   )
 }
-
 export default AddGrocery
